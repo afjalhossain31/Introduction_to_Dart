e087da1c73263f9bc0006c67cfa7940e9d0f0e64
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-31 11:25:16 +0600

    added String

---
+++ string.dart
@@ -0,0 +1,20 @@
+void main(){
+  var str = " Hello Dart Ajke String Practice korbo ";
+  var str1 = "Dart Programming Language";
+  var str2 = " String All topics and method practice";
+
+  print('Lowercase: ${str.toLowerCase()}');
+  print('Uppercase: ${str1.toUpperCase()}');
+
+  print('Trimmed: ${str.trim()}'); // removes leading and trailing whitespace
+
+  print("startsWith ' Check if a string starts with a given substring ': ${str.startsWith(' Hello')}");
+
+print("Index os Dart: ${str.indexOf('Ajke')}");
+
+print("Replace second Dart with flutter : ${str.replaceAll('Hello', 'Flutter')}");
+
+
+
+
+}
